997cf9e271cda940f3450243284b0c51f9308926
Andy Kim <andyjinkim@gmail.com>
2015-11-17 12:41:09 -0800

    index, create, and destroy functions created in controller. routes created.

---
+++ routes/todo_routes.js
@@ -0,0 +1,14 @@
+var todoController = './controllers/todo_controller.js'
+var express        = require('express')
+var todoRoutes     = express.Router()
+
+// routes for getting all to do list items and adding new ones
+todoRoutes.route('/todos')
+  .get(todoController.index)
+  .post(todoController.createItem)
+
+// route to delete certain to do list item
+todoRoutes.route('/todos/:id')
+  .delete(todoController.destroyItem)
+
+module.exports = todoRoutes
