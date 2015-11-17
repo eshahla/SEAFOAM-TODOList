830e6f7425236f1ea8ae8b019cb5df7d28c97bd1
Sam Hager <sam.hager11@gmail.com>
2015-11-17 15:07:58 -0800

    added tony's lecture code commented out

--- public/app.js
+++ public/app.js
@@ -36,3 +36,22 @@ $('.btn').click(function(){
   $star.click(colorStar);
   $checkboxes.click(strikethrough);
 })
+
+//code from tony's lecture
+// $.ajax({
+//   url: 'http://127.0.0.1:3000/api/todos',
+//   method: 'GET',
+//   success: function(data){
+//     console.log(data)
+//   }
+// })
+
+// $.ajax({
+//   url: 'http://127.0.0.1:3000/api/todos',
+//   method: 'POST',
+//   data: { text: "test2"},
+//   dataType: 'application/json',
+//   success: function(data){
+//     console.log(data)
+//   }
+// })
