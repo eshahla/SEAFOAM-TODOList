272f07ff8167e935358d65722acad762d6f5cf91
Sam Hager <sam.hager11@gmail.com>
2015-11-17 16:30:23 -0800

    moved styling to styles.css. Created ajax call to api/todos for GET to show all items in list upon load

--- public/app.js
+++ public/app.js
@@ -4,6 +4,52 @@ $('.btn').click(function(){
   text.val('');
 });
 
+$.ajax({
+  url: '/api/todos',
+  method: 'GET',
+  success: function(data){
+    // console.log(data)
+      data.forEach(function(i){
+        var text = $('.todo');
+        var input = i.item
+        console.log(i.item)
+        $('.checkbox').last().append('<p><div class="checkbox"><label><input type="checkbox" ng-click="deleteTodo(todo._id)">' + input + '</label></div></p>')
+        //
+      })
+    }
+})
+
+// $.ajax({
+//         url: '/movies'
+//         ,method: 'GET'
+//         ,success: function(data){
+//             console.log(data);
+//
+//             data.forEach(function(m){
+//               var nameSpan = '<span class="movie-name">' + "Movie: " + m.name + '</span>'
+//               var ratingSpan = '<span class="movie-rating">' + "Rating: " + m.rating + '</span>'
+//               var showDetails ='<button class="show-details" id="'+ m._id +'" >Show Details</button>'
+//               var li = '<li>' + nameSpan + " | " + ratingSpan + " | " + showDetails + '</li>'
+//               $('.movies-list').append(li)
+//             });
+//             $('.show-details').on('click', function(evt){
+//               console.log('let us make a request to get the details');
+//               var movieId = this.id;
+//               //or console.log(evt.target.id)
+//               $.ajax({
+//                 //or /movies/ + evt.target.id
+//                 url: '/movies/' + movieId
+//                 ,method: 'GET'
+//                 ,success: function(data){
+//                   console.log(data);
+//                 }
+//               })
+//             })
+//         }
+//       })
+//     })
+
+
 //code from tony's lecture
 // $.ajax({
 //   url: 'http://127.0.0.1:3000/api/todos',
